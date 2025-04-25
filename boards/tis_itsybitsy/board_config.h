3756966befeaddf5b3fb882c30e560ca7e068c46
WestfW <westfw@gmail.com>
2025-04-25 14:57:33 -0700

    Add tis_trinket and tis_itsybitsy for Turn Island Systems

---
+++ boards/tis_itsybitsy/board_config.h
@@ -0,0 +1,20 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Turn Island Systems"
+#define PRODUCT_NAME "TIS"
+#define VOLUME_LABEL "TISBOOT"
+#define INDEX_URL "https://TurnIslandSystems.com"
+#define BOARD_ID "SAMD21G18A"
+
+#define USB_VID 0x04D8
+#define USB_PID 0xE4FF
+
+#define LED_PIN PIN_PA17
+
+//#define BOARD_RGBLED_CLOCK_PIN            PIN_PA00
+//#define BOARD_RGBLED_DATA_PIN             PIN_PA01
+
+#endif
---
+++ boards/tis_trinket/board_config.h
@@ -0,0 +1,23 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Turn Island Systems"
+#define PRODUCT_NAME "TIS"
+#define VOLUME_LABEL "TISBOOT"
+#define INDEX_URL "https://TurnIslandSystems.com"
+#define BOARD_ID "SAMD21E18A"
+
+#define USB_VID 0x04D8
+#define USB_PID 0xE4FF
+
+
+#define LED_PIN PIN_PA10
+//#define LED_TX_PIN PIN_PA27
+//#define LED_RX_PIN PIN_PB03
+
+//#define BOARD_RGBLED_CLOCK_PIN            PIN_PA01
+//#define BOARD_RGBLED_DATA_PIN             PIN_PA00
+
+#endif
